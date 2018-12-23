8c549781d959cd9efc6b257f816b5cd6f0174d02
badenko <badenkodmitriy@gmail.com>
2018-12-23 18:46:53 +0200

    Tweaked order model, added order statuses

--- main/models/order.py
+++ main/models/order.py
@@ -3,8 +3,14 @@ from django.db import models
 from .customer import Customer
 
 class Order(models.Model):
+    ORDER_STATUSES = (
+        ('A', 'accepted'),
+        ('D', 'delivered'),
+        ('C', 'canceled')
+    )
     order_number = models.CharField(max_length=50, default=datetime.now().date())
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choises=ORDER_STATUSES, default='A')
     date = models.DateField(auto_now_add=True)
     time = models.CharField(max_length=50, default=datetime.now().time().strftime("%H:%M"))
 
