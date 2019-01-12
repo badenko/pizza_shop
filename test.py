fedaad683dd810fc79a7064b16845babe053aa48
badenko <badenkodmitriy@gmail.com>
2019-01-12 20:23:01 +0200

    Added requirements.txt

--- test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, date, time, timedelta
-
-start_date = date(2018, 9, 1)
-end_date = date.today()
-counter = start_date
-while counter <= end_date:
-    print(counter)
-    counter += timedelta(days=1)
